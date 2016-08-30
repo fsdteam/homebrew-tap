85a9769cc07fc350de32fc8035503f5e498e0c96
Nenad Lukic <nenad@fsd.rs>
2016-08-30 11:27:41 +0200

    Add f-tools initial

---
+++ f-tools.rb
@@ -0,0 +1,9 @@
+class FTools < Formula
+  homepage "https://github.com/fsdteam/f-tools"
+  url "https://github.com/fsdteam/f-tools/archive/0.0.1.tar.gz"
+  sha256 "e3c68c1f836f025f642e4ca045bd6f259d423b15422f4c0194713c028f09309d"
+
+  def install
+    bin.install "f-tools"
+  end
+end
