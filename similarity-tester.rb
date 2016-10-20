26d35677f26bb6cb5ff267528627a9e85d425e71
Nenad Lukic <nenad@fsd.rs>
2016-10-20 15:31:49 +0200

    URL Change

--- similarity-tester.rb
+++ similarity-tester.rb
@@ -1,6 +1,6 @@
 class SimilarityTester < Formula
 homepage "https://github.com/fsdteam/similarity-tester"
-url "https://github.com/fsdteam/similarity-tester/archive/0.0.1.tar.gz"
+url "https://github.com/fsdteam/similarity-tester/archive/0.0.2.tar.gz"
 sha256 "f12b479c2f123078208bf4762fde1789aee9c0107a8b827e02352a57be91499e"
 
 def install
