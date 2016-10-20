b8fcdbcb5600af4aa1aededf2cd4595191ecfcb5
Nenad Lukic <nenad@fsd.rs>
2016-10-20 15:04:41 +0200

    [BUGFIX] Fixed textedit quotes

--- f-tools.rb
+++ f-tools.rb
@@ -1,10 +1,10 @@
 class FTools < Formula
-  homepage "https://github.com/fsdteam/f-tools"
-  url "https://github.com/fsdteam/f-tools/archive/0.0.2.tar.gz"
-  sha256 "e3c68c1f836f025f642e4ca045bd6f259d423b15422f4c0194713c028f09309d"
+homepage "https://github.com/fsdteam/f-tools"
+url "https://github.com/fsdteam/f-tools/archive/0.0.2.tar.gz"
+sha256 "e3c68c1f836f025f642e4ca045bd6f259d423b15422f4c0194713c028f09309d"
 
-  def install
-    bin.install “sim_php”
-    man1.install “sim_php.1”
+def install
+    bin.install "sim_php"
+    man1.install "sim_php.1"
   end
 end
