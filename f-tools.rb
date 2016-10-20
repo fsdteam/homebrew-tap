e7f9e39441147939cfc953987f5b7e8c76de2adf
Nenad Lukic <nenad@fsd.rs>
2016-10-20 15:05:43 +0200

    [BUGFIX] Fixed SHA

--- f-tools.rb
+++ f-tools.rb
@@ -1,7 +1,7 @@
 class FTools < Formula
 homepage "https://github.com/fsdteam/f-tools"
 url "https://github.com/fsdteam/f-tools/archive/0.0.2.tar.gz"
-sha256 "e3c68c1f836f025f642e4ca045bd6f259d423b15422f4c0194713c028f09309d"
+sha256 "2df834caddfed5ea489a137c3522ed07a6b0fc811c4471ce14e536b95e1be737"
 
 def install
     bin.install "sim_php"
