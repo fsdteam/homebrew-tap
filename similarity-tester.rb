f53842ae384f723cd17fa3285b90cce16ceba93d
Nenad Lukic <nenad@fsd.rs>
2016-10-20 15:32:23 +0200

    SHA change

--- similarity-tester.rb
+++ similarity-tester.rb
@@ -1,7 +1,7 @@
 class SimilarityTester < Formula
 homepage "https://github.com/fsdteam/similarity-tester"
 url "https://github.com/fsdteam/similarity-tester/archive/0.0.2.tar.gz"
-sha256 "f12b479c2f123078208bf4762fde1789aee9c0107a8b827e02352a57be91499e"
+sha256 "8b9809e36b40a8c2b89fd5da5b1dcdf7ae2a2a521bf5cbd0519e4eec5790b23c"
 
 def install
     bin.install "sim_php"
