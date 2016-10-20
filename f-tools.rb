45eb976119ac4e78b4440a2aaec57833bd82943c
Nenad Lukic <nenad@fsd.rs>
2016-10-20 14:55:27 +0200

    [FEATURE] Copy new version files to proper locations

--- f-tools.rb
+++ f-tools.rb
@@ -1,9 +1,10 @@
 class FTools < Formula
   homepage "https://github.com/fsdteam/f-tools"
-  url "https://github.com/fsdteam/f-tools/archive/0.0.1.tar.gz"
+  url "https://github.com/fsdteam/f-tools/archive/0.0.2.tar.gz"
   sha256 "e3c68c1f836f025f642e4ca045bd6f259d423b15422f4c0194713c028f09309d"
 
   def install
-    bin.install "f-tools"
+    bin.install “sim_php”
+    man1.install “sim_php.1”
   end
 end
